1853916f9da5774e91cf383f9a0d7661dac55862
kbm1611 <bm1611@naver.com>
2026-02-10 17:29:12 +0900

    DBDao

--- src/model/dao/DBDao.java
+++ src/model/dao/DBDao.java
@@ -7,7 +7,7 @@ public class DBDao {
     public static Connection getConnection() {
         Connection conn = null;
         try{
-            String url = "jdbc:mysql://localhost:3306/boardservice7";
+            String url = "jdbc:mysql://localhost:3306/hwaturo";
             String user = "root"; String password = "1234";
             Class.forName("com.mysql.cj.jdbc.Driver"); // mysql 라이브러리 객체 메모리할당/불러오기
             conn = DriverManager.getConnection( url, user, password );
