fdbb2f4ae3f6f5edb2f36b597f7c9d18052bb99f
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-13 15:42:46 +0900

    feat: 싱글톤 적용

--- src/view/PlayView.java
+++ src/view/PlayView.java
@@ -1,4 +1,10 @@
 package view;
 
+import java.util.Scanner;
+
 public class PlayView {
+    private PlayView(){}
+    private static PlayView instance = new PlayView();
+    public static PlayView getInstance(){return instance;}
+    private Scanner scan = new Scanner(System.in);
 }
