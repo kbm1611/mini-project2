ce9c22545dfb3baf74fe913997c9f37cf42b0a24
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-13 15:37:00 +0900

    feat: 싱글톤 적용

--- src/controller/PlayController.java
+++ src/controller/PlayController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import service.GameService;
+
+import java.util.Scanner;
+
 public class PlayController {
+    private PlayController(){}
+    private static final PlayController instance = new PlayController();
+    public static PlayController getInstance(){return instance;}
+    private GameService GS = GameService.getInstance();
+
+
 }
