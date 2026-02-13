da0ecaa231cd21b9e95d2c910ca8f1d11f06f490
kbm1611 <bm1611@naver.com>
2026-02-13 17:57:13 +0900

    게임 저장/불러오기 기초 설계

--- src/service/GameSaveService.java
+++ src/service/GameSaveService.java
@@ -1,9 +1,16 @@
 package service;
 
+import model.dao.GameLogDao;
+
 public class GameSaveService {
     private GameSaveService(){}
     private static final GameSaveService instance = new GameSaveService();
-    public static GameSaveService getInstance(){
-        return instance;
-    }
+    public static GameSaveService getInstance(){ return instance; }
+    private GameLogDao gld = GameLogDao.getInstance();
+
+
+
+    //저장하는 기능
+
+    //불러오기 기능
 }
