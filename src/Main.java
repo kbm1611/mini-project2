e600e6289c4aff7dd7e4cc217ce62885b0b486b1
kbm1611 <bm1611@naver.com>
2026-02-23 17:36:42 +0900

    0223 Main

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,7 @@
-import constant.GameConst;
-import model.dto.Card;
-import model.dto.Item;
-import model.dto.PlayerDto;
-import service.GameSaveService;
-import service.GameService;
-import view.ShopView;
 import view.UserView;
 
-import java.util.ArrayList;
-
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
         UserView.getInstance().index();
-//        ShopView.getInstance().printShopView();
-/*
-        //게임 기능 테스트
-        GameService gs = GameService.getInstance();
-        PlayerDto player = PlayerDto.getInstance();
-        player.setUser_no(1); //유저번호
-        player.setCurrent_round(2); // 현재 라운드
-        player.setCurrent_hp(3); // 현재 체력
-        player.setCurrent_discard(3); // 현재 버리기 횟수
-        ArrayList<Card> grave = new ArrayList<>();
-        ArrayList<Card> hand = new ArrayList<>();
-        ArrayList<Card> card=  new ArrayList<>();
-        ArrayList<Item> item = new ArrayList<>();
-        card.
-
-        player.setCurrent_grave(); // 현재 무덤
-        player.setCurrent_hand(); // 현재 핸드
-        player.setCurrent_money(200); // 현재 돈
-        player.setCurrent_score(500); // 현재 점수
-        player.setCard(); // 현재 카드
-        player.setItem(); // 현재 아이템
-        */
     }
 }
