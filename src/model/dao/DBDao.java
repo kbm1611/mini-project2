7e158c160c27183022415c47aa70d673322c7bb6
kbm1611 <bm1611@naver.com>
2026-02-24 09:57:57 +0900

    0224 데이터베이스 연동 성공 문구 주석처리

--- src/model/dao/DBDao.java
+++ src/model/dao/DBDao.java
@@ -11,7 +11,7 @@ public class DBDao {
             String user = "root"; String password = "1234";
             Class.forName("com.mysql.cj.jdbc.Driver"); // mysql 라이브러리 객체 메모리할당/불러오기
             conn = DriverManager.getConnection( url, user, password );
-            System.out.println("[준비] 데이터베이스 연동 성공");
+//            System.out.println("[준비] 데이터베이스 연동 성공");
         }catch (Exception e){
             System.out.println("[경고] 데이터베이스 연동 실패 : 관리자에게 문의");
         }
